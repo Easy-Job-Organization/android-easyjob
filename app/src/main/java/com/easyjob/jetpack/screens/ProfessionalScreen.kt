d2c0240c39fb5dfd24a7af2458e26665ccce1869
JuanEstebanDuque <mijuan79e8@gmail.com>
2024-10-20 22:16:04 -0500

    update: professional page in client connect with API

--- app/src/main/java/com/easyjob/jetpack/screens/ProfessionalScreen.kt
+++ app/src/main/java/com/easyjob/jetpack/screens/ProfessionalScreen.kt
@@ -1,5 +1,6 @@
 package com.easyjob.jetpack.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,14 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.sharp.Lock
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -27,10 +29,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.easyjob.jetpack.Comment
-import com.easyjob.jetpack.Service
 import com.easyjob.jetpack.ui.theme.components.ButtonSection
 import com.easyjob.jetpack.ui.theme.components.CommentsCard
 import com.easyjob.jetpack.ui.theme.components.FilterCard
@@ -39,68 +40,34 @@ import com.easyjob.jetpack.ui.theme.components.PrimaryButton
 import com.easyjob.jetpack.ui.theme.components.ProfileSection
 import com.easyjob.jetpack.ui.theme.components.SecondaryButton
 import com.easyjob.jetpack.ui.theme.components.Topbar
+import com.easyjob.jetpack.viewmodels.ProfessionalProfileViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfessionalProfileScreen(navController: NavController = rememberNavController(), id: String? = "NO_ID") {
+fun ProfessionalProfileScreen(
+    navController: NavController = rememberNavController(),
+    professionalProfileViewModel: ProfessionalProfileViewModel = viewModel(),
+    id: String,
+) {
+
+    val professionalState by professionalProfileViewModel.professional.observeAsState()
+    val servicesState by professionalProfileViewModel.services.observeAsState()
+    val reviewsState by professionalProfileViewModel.reviews.observeAsState()
+    Log.e(">>>", professionalState.toString())
+    Log.e(">>>", servicesState.toString())
+    Log.e(">>>", reviewsState.toString())
 
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    LaunchedEffect(id) {
+        professionalProfileViewModel.fetchProfessional(id)
+        professionalProfileViewModel.fetchServicesOfProfessinal(id)
+        professionalProfileViewModel.fetchReviewsOfProfessinal(id)
+    }
 
     var activeSection by remember {
         mutableStateOf(true)
     }
-
-    val profesiones = listOf(
-        Service(
-            title = "Electricista",
-            descriptions = listOf("Montaje de una red electrica en un apartamento pequeno"),
-            prices = listOf(45900.0)
-        ),
-        Service(
-            title = "Plomeria",
-            descriptions = listOf("Asesoria en fugas de agua de viviendas", "Recoga su mangera que no sirve"),
-            prices = listOf(70000.0, 45000.0)
-        ),
-        Service(
-            title = "Plomeria",
-            descriptions = listOf("Asesoria en fugas de agua de viviendas", "Recoga su mangera que no sirve"),
-            prices = listOf(70000.0, 45000.0)
-        ),
-        Service(
-            title = "Plomeria",
-            descriptions = listOf("Asesoria en fugas de agua de viviendas", "Recoga su mangera que no sirve"),
-            prices = listOf(70000.0, 45000.0)
-        ),
-    )
-
-
-    val comentarios = listOf(
-        Comment(
-            name = "Marco Rosa",
-            starts = 4,
-            date = "06 Ago de 1945",
-            description = "Montaje de una red electrica en un apartamento pequeno"
-        ),
-        Comment(
-            name = "Nicolas Do Santos",
-            starts = 1,
-            date = "08 Sep de 2029",
-            description = "Muito ruim, deixei ele sozinho por 5 minutos e ele levou comigo a caixa de dente da minha avó"
-        ),
-        Comment(
-            name = "Byan Cafre",
-            starts = 2,
-            date = "30 Feb de 2012",
-            description = "Se me llevó la licuadora, no lo contraten más"
-        ),
-        Comment(
-            name = "Nicolas Maduro",
-            starts = 2,
-            date = "29 Nov de 2019",
-            description = "Me obligó a pagarle más, y aparte de todo me robó ropa en la cara"
-        ),
-    )
-
 
     Scaffold(
         modifier = Modifier
@@ -127,15 +94,14 @@ fun ProfessionalProfileScreen(navController: NavController = rememberNavControll
         ) {
 
             ProfileSection(
-                image = "https://e7.pngegg.com/pngimages/324/645/png-clipart-pokeball-pokeball-thumbnail.png",
-                descriptionImage = "mclovin",
-                name = "Sebastián Escobar Marín",
+                image = professionalState?.photo_url,
+                descriptionImage = "profile photo",
+                name = "${professionalState?.name} ${professionalState?.last_name}",
                 cityCountry = "Cali, Colombia",
                 iconSize = 16,
                 stars = 4,
                 comments = "444"
             )
-
             Column(modifier = Modifier
                 .padding(vertical = 14.dp)
                 .wrapContentHeight(),
@@ -185,15 +151,15 @@ fun ProfessionalProfileScreen(navController: NavController = rememberNavControll
             Row(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                ButtonSection(active = activeSection, text = "Información", onClick = { activeSection = !activeSection }, width = 185)
-                ButtonSection(active = !activeSection, text = "Opiniones", onClick = { activeSection = !activeSection }, width = 185)
+                ButtonSection(active = activeSection, text = "Información", onClick = { activeSection = !activeSection }, width = 200)
+                ButtonSection(active = !activeSection, text = "Opiniones", onClick = { activeSection = !activeSection }, width = 200)
             }
             Box(modifier = Modifier.height(10.dp))
             Column(modifier = Modifier.fillMaxWidth()) {
                 if (activeSection) {
-                    InformationCard(services = profesiones)
+                    servicesState?.let { InformationCard(services = it) }
                 } else {
-                    CommentsCard(comments = comentarios)
+                    reviewsState?.let { CommentsCard(reviews = it) }
                 }
             }
 
